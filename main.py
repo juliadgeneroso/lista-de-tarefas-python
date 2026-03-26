d276815ac7fcc2ceec435627d961a4f27d031e6b
juliadgeneroso <juliadgeneroso@gmail.com>
2026-03-25 23:16:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-tarefas = [ ]
+tarefas = []
 
 while True:
-  print("\n--- Lista de Tarefas ---")
-  print("1. Adicionar tarefa")
-  print("2. Ver tarefas")
-  print("3. Remover tarefa")
-  print("4. Sair")
+    print("\n--- Lista de Tarefas ---")
+    print("1. Adicionar tarefa")
+    print("2. Ver tarefas")
+    print("3. Remover tarefa")
+    print("4. Sair")
 
-opcao = input("Escolha uma opção: ")
+    opcao = input("Escolha uma opção: ")
 
-if opcao == "1":
-  tarefa = input("Digite a tarefa: ")
-  tarefas.append(tarefa)
-  print ("Tarefa adicionada!")
+    if opcao == "1":
+        tarefa = input("Digite a tarefa: ")
+        tarefas.append(tarefa)
+        print("Tarefa adicionada!")
 
-elif opcao == "2":
-  if not tarefas:
-    print("Nenhuma tarefa encontrada.")
-  else:
-    for i, tarefa in enumerate(tarefas, 1):
-      print(f"{i}. {tarefa}")
+    elif opcao == "2":
+        if not tarefas:
+            print("Nenhuma tarefa encontrada.")
+        else:
+            for i, tarefa in enumerate(tarefas, 1):
+                print(f"{i}. {tarefa}")
 
-elif opcao == "3":
-  num = int(input("Número da tarefa para remover: "))
-  if 0 < num <= len(tarefas):
-    tarefas.pop(num - 1)
-    print("Tarefa removida!")
-  else:
-    print("Número inválido.")
+    elif opcao == "3":
+        num = int(input("Número da tarefa para remover: "))
+        if 0 < num <= len(tarefas):
+            tarefas.pop(num - 1)
+            print("Tarefa removida!")
+        else:
+            print("Número inválido.")
 
-elif opcao == "4":
-  print("Saindo...")
-break
+    elif opcao == "4":
+        print("Saindo...")
+        break
 
-else:
-  print("Opção inválida.")
-  
+    else:
+        print("Opção inválida.")
